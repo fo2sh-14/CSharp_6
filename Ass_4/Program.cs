1c31c2021aa9f461fe94226e902a04f2e7229b67
youssef fouad <yfo2sh@outlook.com>
2026-09-19 14:16:58 +0300

    1st answer

--- Ass_4/Program.cs
+++ Ass_4/Program.cs
@@ -4,8 +4,12 @@
     {
         static void Main(string[] args)
         {
+            #region 1st answer
             Book book_1 = new Book();
-            Console.WriteLine(book_1.password);
+            Console.WriteLine(book_1.password); // error 
+            // Because private scope in class book only (not main)
+            #endregion
+
         }
     }
 }
