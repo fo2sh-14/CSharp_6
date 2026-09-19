1c8dc298972846c60f110ab459656d77aed591ac
youssef fouad <yfo2sh@outlook.com>
2026-09-19 14:14:38 +0300

    Add project files.

---
+++ Ass_4/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Ass_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book_1 = new Book();
+            Console.WriteLine(book_1.password);
+        }
+    }
+}
